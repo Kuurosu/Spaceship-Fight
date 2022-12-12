56624e3f1020a02b9fc82ad10e5ce5efee7bfc7a
Kuurosu <robcakes69@gmail.com>
2022-12-12 17:11:49 +0000

    Add ship class and draw function to act as the ship

--- run.py
+++ run.py
@@ -35,6 +35,20 @@ BACKGROUND = pygame.transform.scale(pygame.image.load(
     os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50), 0)
+
+
 def main():
     """
     Starts the game with a consistent 60fps
