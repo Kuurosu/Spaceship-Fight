cbd8e3d8faa65f5fb124c32113a4c42af56ff6a5
Kuurosu <robcakes69@gmail.com>
2022-12-12 16:30:03 +0000

    Add enemy and player ship

--- run.py
+++ run.py
@@ -3,4 +3,15 @@ import os
 import time
 import random
 
+# Loads enemy ships
+RED_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# Loads player ships
+YELLOW_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_yellow_small.png"))
 
